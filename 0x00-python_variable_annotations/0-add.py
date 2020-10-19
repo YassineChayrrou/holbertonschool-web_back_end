a6dd90329b1f0c193b799fbcd777deaec069bd7a
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-19 16:52:56 +0100

    documented models check

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""type annotated function that add 2 floats and returns float"""
 
 def add(a: float, b: float) -> float:
+    """add: adds two floats"""
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""concatinates two strings"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """concat: concatinate 2 strings"""
     return str1 + str2
