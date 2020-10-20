940c3792b8cdd898e7cb7ebedb593ba8b3ae21c3
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-20 22:53:24 +0100

    Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""python async basics task 1"""
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int, n: int) -> List[float]:
+    """wait_n: spawn wait_random n times with delay equals to max_delay"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    record = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        record.append(delay)
+    return record
