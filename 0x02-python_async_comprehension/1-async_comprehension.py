2b12559f4ea718286a90e5aa91fd3b566317f7ec
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-21 00:50:28 +0100

    Task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""python async comprehension"""
+
+
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """async_comprehension: returns a list of numbers passed from async func
+       using async list comprehension
+    """
+    return [i async for i in async_generator()]
