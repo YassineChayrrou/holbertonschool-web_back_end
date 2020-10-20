d270d0a7313d0096e2d89360318cbfbc4334d9aa
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-21 00:37:51 +0100

    Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""python async comprehension"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """async_generator: yields random number between 0 and 10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        randomize = random.random() * 10
+        yield randomize
