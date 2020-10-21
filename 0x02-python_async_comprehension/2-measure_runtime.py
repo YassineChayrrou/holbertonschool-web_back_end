ec0108aee75769d35a12796d71dd195a99362762
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-21 16:53:04 +0100

    Fixed argument passed to asyncio.gather as it needs unpacked set

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,10 +9,14 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """measure_runtime: mesures total runtime"""
     s = time.perf_counter()
     taskList = [asyncio.create_task(async_comprehension()) for i in range(4)]
-    await asyncio.gather(taskList)
+#    print(taskList)
+#    print(*taskList)
+# The asterik is for unpacking the elements in the list to pass to gather as
+# mentioned in documentation
+    await asyncio.gather(*taskList)
     elapsed = time.perf_counter() - s
     return elapsed
