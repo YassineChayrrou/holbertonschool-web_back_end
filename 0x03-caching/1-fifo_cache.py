42b26590d2f8e4cc5a23e6eb21772bcad5f0d9a5
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-26 14:49:16 +0100

    Task 1

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""FIFP caching model"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns item value to key in self.cache_data
+           Deletes first element in self.cache_data if elements count is bigger
+           then MAX_ITEMS
+        """
+        if key and item:
+            self.cache_data[key] = item
+            while len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first = list(self.cache_data.keys())
+                self.cache_data.pop(first[0])
+                print("DISCARD: {}".format(first[0]))
+        pass
+
+    def get(self, key):
+        """returns the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
