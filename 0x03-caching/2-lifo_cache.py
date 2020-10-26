cb45503154add0a0df1168028bdee1e5c5a8d977
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-10-26 20:52:16 +0100

    Task 2

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""LIFO caching model"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache inherits from BaseCaching"""
+
+    def __init__(self):
+        """init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns item value to key in self.cache_data
+           Deletes first element in self.cache_data if elements count is bigger
+           then MAX_ITEMS
+        """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(self.cache_data.popitem()))
+
+    def get(self, key):
+        """returns the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
