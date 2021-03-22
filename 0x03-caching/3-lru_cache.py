5002d0f8970511d4678a954268c4c9ff3656bbec
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-03-22 19:26:13 +0100

    changed lru_cache logic from dictionary to list

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -8,11 +8,11 @@ BaseCaching = __import__("base_caching").BaseCaching
 class LRUCache(BaseCaching):
     """LRUCache inherits from BaseCaching"""
 
-    LRU_Dictionary = {}
 
     def __init__(self):
         """init method"""
         super().__init__()
+        self.lru_cache = []
 
     def put(self, key, item):
         """Assigns item value to key in self.cache_data
@@ -21,26 +21,20 @@ class LRUCache(BaseCaching):
         """
         if key is None or item is None:
             return
+
         self.cache_data[key] = item
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            if LRUCache.LRU_Dictionary:
-                print(LRUCache.LRU_Dictionary)
-                my_key = None
-                my_value = list(LRUCache.LRU_Dictionary.values())[0]
-                for lru_key in LRUCache.LRU_Dictionary:
-                    if my_value >= LRUCache.LRU_Dictionary[lru_key]:
-                        my_key = lru_key
-                        my_value = LRUCache.LRU_Dictionary[lru_key]
-                self.cache_data.pop(my_key)
-                LRUCache.LRU_Dictionary.pop(my_key)
-                print(f"Discard: {my_key}")
+        if key not in self.lru_cache:
+            self.lru_cache.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            my_key = self.lru_cache.pop(0)
+            self.cache_data.pop(my_key)
+            print(f"Discard: {my_key}")
 
     def get(self, key):
         """returns the value in self.cache_data linked to key"""
         if key is None or key not in self.cache_data.keys():
             return None
-        if key not in LRUCache.LRU_Dictionary.keys():
-            LRUCache.LRU_Dictionary[key] = 1
-        else:
-            LRUCache.LRU_Dictionary[key] += 1
+        if key in self.lru_cache:
+            recent_key = self.lru_cache.pop(self.lru_cache.index(key))
+            self.lru_cache.append(recent_key)
         return self.cache_data[key]
