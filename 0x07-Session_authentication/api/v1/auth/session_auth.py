54a63b2e74ddb531a5e47473f5fc1732d7ff262f
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-06 18:47:24 +0100

    Task 2: Empty session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for the API
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class
+    """
+    def __init__(self):
+        pass
