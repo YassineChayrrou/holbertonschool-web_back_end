7090d4954e3837cc9509324af41724625c180b20
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-06 22:17:07 +0100

    nofix for badly formed uuid hexadecimal string

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -15,7 +15,8 @@ class SessionAuth(Auth):
         pass
 
     def create_session(self, user_id: str = None) -> str:
-        """ descriptionLater """
+        """ Creates session and returns session_id
+        """
         if user_id is None:
             return None
         if type(user_id) is not str:
@@ -23,3 +24,14 @@ class SessionAuth(Auth):
         session_id = uuid.uuid4()
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns user_id for a given session_id
+        """
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        session_id = uuid.UUID(session_id)
+        user_id = self.user_id_by_session_id.get(bytes=session_id.bytes)
+        return user_id
