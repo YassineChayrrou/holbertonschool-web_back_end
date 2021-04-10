39dca33e82d4c438ac9e134c442b1048222e564f
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-10 22:52:12 +0100

    new com

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-# """SessionDBAuth module
-# """
-# 
-# 
-# from api.v1.auth.session_exp_auth import SessionExpAuth
-# from models.user_session import UserSession
-# 
-# 
-# class SessionDBAuth(SessionExpAuth):
-#     """ SessionDBAuth class
-#     """
-#     def create_session(self, user_id=None):
-#         """ Overloads SessionExpAuth create_session
-#         """
-#         pass
+"""SessionDBAuth module
+"""
+
+
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ SessionDBAuth class
+    """
+    def create_session(self, user_id=None):
+        """ Overloads SessionExpAuth create_session
+        """
+    if user_id is None:
+        return None
+
+    session_id = super().create_session(user_id)
---
+++ 0x07-Session_authentication/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Test for session db storage module"""
+
+
+from api.v1.auth.session_db_auth import SessionDBAuth
