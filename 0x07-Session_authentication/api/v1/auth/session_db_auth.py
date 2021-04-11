bb2b40b8328c518ef10d8a0eaaf72702a24638b8
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-11 23:10:21 +0100

    Advanced Task: store session in database

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -6,13 +6,14 @@
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 from flask import request
-from models.base import DATA
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
     """ SessionDBAuth class
     """
     def __init__(self):
+        super().__init__()
         self.user_session = UserSession()
 
     def create_session(self, user_id=None):
@@ -30,14 +31,19 @@ class SessionDBAuth(SessionExpAuth):
         """ Overloads method in SessionExpAuth
             Returns User ID by requesting UserSession in DB using session_id
         """
-        super().user_id_for_session_id()
         if session_id is None:
             return None
         session_list = UserSession.search({'session_id': session_id})
         if len(session_list) < 1:
             return None
-        current_session_user_id = session_list[0].user_id
-        return current_session_user_id
+        current_session = session_list[0]
+        local_session_dict = self.user_id_by_session_id
+        created_at = local_session_dict.get(session_id).get('created_at')
+        expiration = created_at + timedelta(seconds=self.session_duration)
+        if expiration < datetime.now():
+            current_session.remove()
+            return None
+        return current_session.user_id
 
     def destroy_session(self, request=None):
         """ Overloads method in SessionExpAuth
--- 0x07-Session_authentication/test.py
+++ 0x07-Session_authentication/test.py
@@ -2,4 +2,43 @@
 """Test for session db storage module"""
 
 
+from flask import Flask, request
+from api.v1.auth.session_auth import SessionAuth
+from models.user import User
 from api.v1.auth.session_db_auth import SessionDBAuth
+
+""" Create a user test """
+user_email = "bobsession@hbtn.io"
+user_clear_pwd = "fake pwd"
+
+user = User()
+user.email = user_email
+user.password = user_clear_pwd
+user.save()
+
+""" Create a session ID """
+sa = SessionDBAuth()
+session_id = sa.create_session(user.id)
+print("User with ID: {} has a Session ID: {}".format(user.id, session_id))
+print("--------------------------------------------------------------")
+""" User ID For Session ID """
+user_id = sa.user_id_for_session_id(session_id)
+print(f"Session with ID: {session_id} has a User ID: {user_id}")
+
+
+""" Create a Flask app """
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    request_user = sa.destroy_session(request)
+    if request_user is None:
+        return "No session found\n"
+    return "Result: {}\n".format(request_user)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
