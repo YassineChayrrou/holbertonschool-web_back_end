196f9a91d353853a2fa63e0c59687611972edb89
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-13 17:25:08 +0100

    type annotations check and restore import

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,7 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from typing import TypeVar
+from sqlalchemy.orm.session import Session
 from user import Base, User
 
 
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ adds User to database
         """
         new_user = User(email=email, hashed_password=hashed_password)
