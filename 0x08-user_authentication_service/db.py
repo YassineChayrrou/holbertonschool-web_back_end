1316fb1d39f17d50728e79075d6c0a7e47491767
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-13 17:20:36 +0100

    deleted no used import

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,7 +4,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from typing import TypeVar
 from user import Base, User
 
