20c767c49162660f29a648f12db2550022d7982e
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-15 17:56:35 +0100

    check user not None

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -53,8 +53,11 @@ class DB:
         """ Updates user based on id and commit changes to DB
         """
         user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            setattr(user, key, value)
-        self._session.commit()
+        if user:
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError
+                else:
+                    setattr(user, key, value)
+            self._session.commit()
+        return None
