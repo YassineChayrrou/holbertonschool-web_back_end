b0a64b2aa2f24b5353833d494de4828936697ab2
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-16 23:48:39 +0100

    Task 7: set user registration on flask app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,11 +2,12 @@
 """ Flask app module """
 
 
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -14,5 +15,19 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=['POST'])
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = auth.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+    return jsonify({
+        "email": email,
+        "message": "user created"
+        })
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
