9290c1c2eebf60d902e748a28f5b8552e6d34ba3
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-17 07:26:59 +0100

    Task 17: get reset password token

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -81,5 +81,18 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=['POST'])
+def get_reset_password_token():
+    """
+    get_reset_password_token - resets password token flask implementation
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except Exception as e:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
