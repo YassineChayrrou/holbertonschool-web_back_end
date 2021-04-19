d5618e279bf99dfaff63577a8cc97bd603654ca5
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-04-20 00:36:39 +0100

    Task 1: parameterize unit test - assertRaises

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test_access_nested_map - test method
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ test_access_nested_map_exception - test method """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
