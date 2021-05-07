e2fd89d45955002655a17688df5504789131fcb2
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-07 12:11:07 +0100

    Task 11: Setting up view

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,13 @@
+-- SQL script
+-- creates a view need_meeting
+-- lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
+CREATE VIEW need_meeting
+AS SELECT name FROM students
+WHERE
+score < 80
+AND
+(
+    last_meeting < ADDDATE(CURRENT_DATE(), INTERVAL -1 MONTH)
+    OR
+    last_meeting IS NULL
+);
