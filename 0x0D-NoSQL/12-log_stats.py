07f5550b068d94681dda47a2e848c77e1a472cb7
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-11 10:40:45 +0100

    documentation

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -7,23 +7,33 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.logs
-mongo_collection = db.nginx
+collection = db.nginx
 
-GET = mongo_collection.count_documents({"method": "GET"})
-POST = mongo_collection.count_documents({"method": "POST"})
-PUT = mongo_collection.count_documents({"method": "PUT"})
-PATCH = mongo_collection.count_documents({"method": "PATCH"})
-DELETE = mongo_collection.count_documents({"method": "DELETE"})
-STATUS = mongo_collection.count_documents({"method": "GET", "path": "/status"})
-LOGS = mongo_collection.count_documents({})
-screen = f"""
-{LOGS} logs
-Methods:
-    method GET: {GET}
-    method POST: {POST}
-    method PUT: {PUT}
-    method PATCH: {PATCH}
-    method DELETE: {DELETE}
-{STATUS} status check
-"""
-print(screen.strip())
+
+def log_stats(mongo_collection) -> None:
+    """
+    log_stats: returns a string with nginx logs stats from mongodb
+    """
+    GET = mongo_collection.count_documents({"method": "GET"})
+    POST = mongo_collection.count_documents({"method": "POST"})
+    PUT = mongo_collection.count_documents({"method": "PUT"})
+    PATCH = mongo_collection.count_documents({"method": "PATCH"})
+    DELETE = mongo_collection.count_documents({"method": "DELETE"})
+    STATUS = mongo_collection.count_documents({
+                                                "method": "GET",
+                                                "path": "/status"
+                                            })
+    LOGS = mongo_collection.count_documents({})
+    print(f"{LOGS} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {GET}")
+    print(f"\tmethod POST: {POST}")
+    print(f"\tmethod PUT: {PUT}")
+    print(f"\tmethod PATCH: {PATCH}")
+    print(f"\tmethod DELETE: {DELETE}")
+    print(f"{STATUS} status check")
+
+
+if __name__ == "__main__":
+
+    log_stats(collection)
