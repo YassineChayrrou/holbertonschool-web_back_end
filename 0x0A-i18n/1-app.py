f4950cebcd2b7a4957eaa830eb32467208918067
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-16 09:29:05 +0100

    Task 1: Babel config DEFAULT_LOCALE and TIMEZONE

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Python Flask Module """
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Babel Config class"""
+
+    LANGUAGES = ["en", "fr"]
+
+app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
+
+
+@app.route("/")
+def index():
+    """Index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
