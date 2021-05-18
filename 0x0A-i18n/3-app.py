5878fc39c593bdeeed4b7a8ad1ceffa731dffc64
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-18 17:12:03 +0100

    Task 3: parametrize templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -3,32 +3,32 @@
 
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 
 class Config:
     """ Babel Config class """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
-app.config.from_object('1-app.Config')
+app.config.from_object("1-app.Config")
 babel = Babel(app)
 
 
-@app.route('/')
-def index():
-    """handle home route index page"""
-    return render_template('3-index.html')
+@app.route("/")
+def index_view():
+    """ Handle Home route """
+    return render_template("3-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """ get best matched locale supported language """
+    """ returns best matching language from request """
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
