45738de27cc6f6c42f3d8d4e29c384cda37bc1b8
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-18 12:00:50 +0100

    Task 1: Basic Babel setup

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -6,23 +6,24 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config:
     """Babel Config class"""
-
     LANGUAGES = ["en", "fr"]
+    TIMEZONE = "UTC"
 
 
 app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
+app.config['BABEL_DEFAULT_TIMEZONE'] = Config.TIMEZONE
+babel = Babel(app)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     """Index page"""
     return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
