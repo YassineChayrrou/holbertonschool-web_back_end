ac9f7958f0f3647ff0005b25ddaef07fc31438fe
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-18 16:50:09 +0100

    documenting all modules

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -7,7 +7,7 @@ from flask_babel import Babel, gettext
 
 
 class Config:
-    """Babel Config class"""
+    """ Babel Config class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,16 +18,16 @@ app.config.from_object('1-app.Config')
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """ get best matched locale supported languages """
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
 @app.route('/')
 def index():
     """handle home route index page"""
     return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ get best matched locale supported language """
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
