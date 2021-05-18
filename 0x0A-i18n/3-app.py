7f677ebef4682cf9b645f2e353b14e89d5fcf69a
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-05-18 12:57:51 +0100

    Task 3: parametrize templates

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Python Flask Module """
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    """Babel Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object('1-app.Config')
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ get best matched locale supported languages """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """Index page"""
+    home_title = gettext(u'Welcome to Holberton')
+    home_header = gettext(u'Hello world')
+    return render_template('2-index.html', home_title=home_title, home_header=home_header)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
