4b4aed4d86804583dc9e63b035e520b3413c667a
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-09 17:25:33 +0100

    check plz

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,6 +1,19 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return [{ status: 'fulfilled', value: await signUpUser(firstName, lastName).then((data) => data) }, { status: 'rejected', value: await uploadPhoto(fileName).catch((error) => error.toString()) }];
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  return [
+    {
+      status: "fulfilled",
+      value: await signUpUser(firstName, lastName).then((data) => data),
+    },
+    {
+      status: "rejected",
+      value: await uploadPhoto(fileName).catch((error) => error.toString()),
+    },
+  ];
 }
