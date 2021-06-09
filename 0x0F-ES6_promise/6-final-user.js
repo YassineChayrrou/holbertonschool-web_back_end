1fd6d4e6b39254ae6e5280c717f3b61fe42bf7ed
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-09 17:13:06 +0100

    eslint check

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,17 +1,15 @@
 import uploadPhoto from "./5-photo-reject";
 import signUpUser from "./4-user-promise";
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
-  const signedUser = await signUpUser(firstName, lastName).then((data) => ({
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signedUser = await signUpUser(firstName, lastName)
+  .then((data) => ({
     status: "fulfilled",
     value: data,
   }));
 
-  const profilePhoto = await uploadPhoto(fileName).catch((error) => ({
+  const profilePhoto = await uploadPhoto(fileName)
+  .catch((error) => ({
     status: "rejected",
     value: error.toString(),
   }));
