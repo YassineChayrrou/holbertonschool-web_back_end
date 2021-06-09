7205360271ca3ce89657607b3080ee9a2f3ba783
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-09 18:54:33 +0100

    catch error

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,9 @@
+export default function guardrail(mathFunction) {
+  const  queue = [];
+  let res;
+  try {
+    res = mathFunction();
+  } catch (err) { res = err.toString(); }
+  queue.push(res, 'Guardrail was processed')
+  return queue;
+}
