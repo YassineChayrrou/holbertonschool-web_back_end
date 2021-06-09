6db1b9edb86000076ebd0b8880e14ba176d16df3
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-09 12:44:52 +0100

    2-then.js

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   console.log("Got a response from the API");
   return promise
-    .then(() => {
-      return { status: 200, body: "success" };
-    })
+    .then(() => ({
+        status: 200,
+        body: 'success',
+    }))
     .catch(() => new Error());
 }
