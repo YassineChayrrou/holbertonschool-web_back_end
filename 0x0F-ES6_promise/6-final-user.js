4aa474263f70cda61a71ffbb49eaafc39d44b8e3
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-09 16:58:17 +0100

    task 6: Handle multiple promises

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import uploadPhoto from "./5-photo-reject";
+import signUpUser from "./4-user-promise";
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const array = [];
+  const signedUser = await signUpUser(firstName, lastName).then((data) => ({
+    status: "fulfilled",
+    value: data,
+  }));
+
+  const profilePhoto = await uploadPhoto(fileName).catch((error) => ({
+    status: "rejected",
+    value: error.toString(),
+  }));
+  return array.push(signedUser, profilePhoto);
+}
