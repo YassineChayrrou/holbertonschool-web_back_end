9a55f496af4f083224b6be453029959c3f29100b
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-09 19:15:10 +0100

    9-try.js

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -1,8 +1,8 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    const value = mathFunction();
-    queue.push(value, 'Guardrail was processed');
+    const result = mathFunction();
+    queue.push(result, 'Guardrail was processed');
   } catch (err) {
     queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
