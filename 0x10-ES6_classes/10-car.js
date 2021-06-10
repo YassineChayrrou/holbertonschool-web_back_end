701f8774e6ad09870925665ecac11a363a42e4d2
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-06-10 19:06:13 +0100

    Task 10: return instance of class

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+export default class car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+    // [Symbol.species]
+  }
+}
